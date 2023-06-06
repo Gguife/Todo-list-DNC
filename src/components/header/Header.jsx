d9efd58a5243f5347e8900afc8f0bea7f68987cf
Guilherme Gomes <gguife747@gmail.com>
2023-06-05 23:05:59 -0300

    Finish Header and reposive Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.scss'
+import Logo from '../../assets/eucartoon.png'
 
 export const Header = () => {
   return (
@@ -6,6 +7,9 @@ export const Header = () => {
       <div className="header-content">
         <p>Organização</p>
         <span>Tarefas</span>
+      </div>
+      <div className='header-logo'>
+        <a href="https://github.com/Gguife" target='_blank'><img src={Logo} alt="Logo"/></a>
       </div>
     </section>
   )
